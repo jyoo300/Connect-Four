d940b93be233a85502216f8a188d2fbf2bac131e
John Yoo <32530650+jyoo300@users.noreply.github.com>
2019-10-05 16:26:28 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package hw4;
+
+//main page of the app
+public class main {
+	public static void main(String args[]) {
+		//implements a game with the smart AI
+		GUICF gui = new GUICF(new JohnAI());
+
+		//implements a game with the random AI
+		//GUICF gui = new GUICF(new Random());
+	}
+}
